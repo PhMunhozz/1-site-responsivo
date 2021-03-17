eca5416963f6144c654bc7a44f8c0dcb5b80038b
Paulo Munhoz <paulo.henriquemcf@gmail.com>
2021-03-16 21:15:24 -0300

    :construction: Criando efeitos no JS (em processo)

--- js/responsive-nav.js
+++ js/responsive-nav.js
@@ -1,5 +1,23 @@
+var lastScroll = 0;
+
+window.onscroll = function () {
+  
+    if(document.documentElement.scrollTop > lastScroll){
+      document.getElementsByClassName('cabeca')[0].style.display='none';
+      document.getElementById('myTopnav').style.top='0';
+      lastScroll = document.documentElement.scrollTop;
+    }
+    
+    else {
+      document.getElementsByClassName('cabeca')[0].style.display='block';
+      document.getElementById('myTopnav').style.top='30px';
+      lastScroll = document.documentElement.scrollTop;
+    }
+  }
+
+
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
+function abrirNav() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
       x.className += " responsive";
